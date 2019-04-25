ce9e92fc998372bc3937bd5bb48c9385ae6d0a6e
Owain Roberts <owain.roberts@bjss.com>
2019-04-25 16:55:04 +0100

    EE-16843 Changing DOMAIN_NAME to be new IP Stats one

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ else
     export KC_REALM=pttg-qa
 fi
 
-export DOMAIN_NAME=ip.${DNS_PREFIX}pttg.homeoffice.gov.uk
+export DOMAIN_NAME=ipstats.${DNS_PREFIX}pttg.homeoffice.gov.uk
 
 echo "DOMAIN_NAME is $DOMAIN_NAME"
 
