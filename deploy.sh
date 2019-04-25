f3e0f2733157549a6bbffa6cd45b9cb8dcd946e3
Owain Roberts <owain.roberts@bjss.com>
2019-04-25 15:05:07 +0100

    EE-16843 First commit of basic .gitignore, a deploy script and necessary kd files

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+# #!/bin/bash
+
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
+fi
+
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    export KUBE_TOKEN=${PTTG_IP_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_IP_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_IP_DEV}
+    fi
+fi
+#
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+cd kd
+
+kd --insecure-skip-tls-verify \
+    -f networkPolicy.yaml \
+    -f deployment.yaml \
+    -f service.yaml \
+    -f ingress.yaml
